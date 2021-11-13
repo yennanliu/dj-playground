c538cd9a6921f33e55b526db4e28b82c2a527a2b
yennanliu <f339339@gmail.com>
2021-11-13 16:58:01 +0800

    update

--- social-network/doc/progress.md
+++ social-network/doc/progress.md
@@ -2,6 +2,7 @@
 
 - 2021-10-13 (OK)
 	- https://www.youtube.com/watch?v=ozr6NEomLQw&list=PLgjw1dR712joFJvX_WKIuglbR1SNCeno1&index=1
-- 2021-11-13
+- 2021-11-13 (OK)
 	- https://www.youtube.com/watch?v=dJqWO-lSgWY&list=PLgjw1dR712joFJvX_WKIuglbR1SNCeno1&index=2
-		- 5:54
+- next
+	- https://www.youtube.com/watch?v=wq7f_kbqV4U&list=PLgjw1dR712joFJvX_WKIuglbR1SNCeno1&index=3
--- social-network/src/profiles/models.py
+++ social-network/src/profiles/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.template.defaultfilters import slugify
+from .utils import get_random_code
 
+
+# Create your models here.
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default="no bio", max_length=3000)
-    email= models.EmailField(max_length=200, blank=True)
+    email = models.EmailField(max_length=200, blank=True)
     country = models.CharField(max_length=200, blank=True)
     avatar = models.ImageField(default='avatar.png', upload_to='')
     friends = models.ManyToManyField(User, blank=True, related_name='friends')
@@ -17,3 +20,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}--{self.created}"
+
+    # avoid duplicated slugs
+    def save(self, *args, **kwargs):
+        ex = False
+        if self.first_name and self.last_name:
+            to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
+            ex = Profile.objects.filter(slug=to_slug).exists()
+            while ex:
+                to_slug = slugify(to_slug + " " + str(get_random_code()))
+                ex = Profile.objects.filter(slug=to_slug).exists()
+        else:
+            to_slug = str(self.user)
+        self.to_slug = to_slug
+        super().save(*args, **kwargs)
