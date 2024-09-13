6ea4e8b2b394ada3616b24a8b441ce98f1b45269
yennanliu <f339339@gmail.com>
2024-09-13 20:03:25 +0800

    update

--- chat-room/mysite/chat/routing.py
+++ chat-room/mysite/chat/routing.py
@@ -1,7 +1,15 @@
+# https://channels.readthedocs.io/en/latest/tutorial/part_2.html
+from os import path
+
 from django.urls import re_path
 
 from . import consumers
 
+# websocket_urlpatterns = [
+#     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+# ]
+
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
--- chat-room/mysite/mysite/settings.py
+++ chat-room/mysite/mysite/settings.py
@@ -28,7 +28,6 @@ CHANNEL_LAYERS = {
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -43,6 +42,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -82,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -92,7 +91,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -112,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -123,7 +120,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
